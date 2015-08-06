86bf298be8d9d7520d700f9102c223f4a3a0b4d7
Nicolas Ronsmans <nicolas.ronsmans@orbitz.com>
2015-08-06 12:35:39 -0300

    Add connect, watch and bump support

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,9 +2,33 @@
 
 module.exports = function (grunt) {
   grunt.initConfig({
-    // TODO
+
+    connect: {
+      all: {
+        options: {
+          hostname: '*',
+          port: process.env.PORT || '6789',
+          base: '.',
+          livereload: true
+        }
+      }
+    },
+
+    watch: {
+      general: {
+        files: ['index.html', 'tests/**/*.js'],
+        options: {
+          livereload: true
+        }
+      }
+    }
 
   });
 
-  //TODO
+  grunt.loadNpmTasks('grunt-contrib-bump');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('start', ['connect', 'watch']);
+  grunt.registerTask('default', ['start']);
 };
